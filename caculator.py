42019937465835ca1c129b6d03e0a431c2d72f8d
Karthikeyan <artistkarthikeyan14@gmail.com>
2020-05-03 15:47:17 +0530

    All-func

--- caculator.py
+++ caculator.py
@@ -2,19 +2,19 @@
 
 # Function to add two numbers 
 def add(num1, num2): 
-	return pass
+	return num1 + num2
 
 # Function to subtract two numbers 
 def subtract(num1, num2): 
-	return pass 
+	return num1 - num2 
 
 # Function to multiply two numbers 
 def multiply(num1, num2): 
-	return pass
+	return num1 * num2
 
 # Function to divide two numbers 
 def divide(num1, num2): 
-	return pass
+	return num1 / num2
 
 print("Please select operation -\n" \ 
 		"1. Add\n" \ 
