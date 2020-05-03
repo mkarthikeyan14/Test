2e2401b29a926dbe3fbc16dc1b5dff495da08966
Karthikeyan <artistkarthikeyan14@gmail.com>
2020-05-03 14:30:30 +0530

    mutiply

--- caculator.py
+++ caculator.py
@@ -10,7 +10,7 @@ def subtract(num1, num2):
 
 # Function to multiply two numbers 
 def multiply(num1, num2): 
-	return num1 * num2 
+	return pass
 
 # Function to divide two numbers 
 def divide(num1, num2): 
